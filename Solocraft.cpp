12f88336fa31ca10b04a4f1919da8b280e1a8bc2
Chris Luby <cluby@adobe.com>
2016-05-16 03:12:49 -0600

    Manually manage stats bonus instead of trying to use aura

--- Solocraft.cpp
+++ Solocraft.cpp
@@ -1,81 +1,21 @@
+#include <map>
+
 #include "Config.h"
 #include "ScriptMgr.h"
+#include "Unit.h"
 #include "Player.h"
+#include "Pet.h"
 #include "Map.h"
 #include "Group.h"
 #include "InstanceScript.h"
 
-namespace {
-
-class solocraft_aura_base : public AuraScript {
-	/*
-	bool IsPassive() const override {
-		return true;
-	}
-
-	bool IsDeathPersistent() const override {
-		return true;
-	}
-
-	bool IsPermanent() const override {
-		return true;
-	}
-	*/
-};
-
 /*
-Aura for 5man instances.
-SQL:
-REPLACE INTO spell_dbc SET Id = 200000,
-	Effect1 = 6, #APPLY_AURA
-	Comment = "Solocraft: Aura 5man";
-REPLACE INTO spell_script_names VALUES(200000, "solocraft_aura_5man");
-*/
-class solocraft_aura_5man : public SpellScriptLoader {
-public:
-	static const uint32 SPELL_ID = 200000;
+ * TODO:
+ * 1. Dispel target regeneration
+ * 2. Provide unlimited http://www.wowhead.com/item=17333/aqual-quintessence
+ */
 
-	solocraft_aura_5man() : SpellScriptLoader("solocraft_aura_5man") {
-		TC_LOG_INFO("scripts.solocraft.aura", "[Solocraft] solocraft_aura_5man Loaded");
-	}
-
-	class solocraft_aura_5man_AuraScript : public solocraft_aura_base {
-		PrepareAuraScript(solocraft_aura_5man_AuraScript);
-
-        void AfterApply(AuraEffect const *aurEff, AuraEffectHandleModes mode) {
-            if (GetCaster()) {
-                if (Unit* owner = GetCaster()->GetOwner()) {
-                	if (Player *player = owner->ToPlayer()) {
-                		ChatHandler(player->GetSession()).SendSysMessage("You have been buffed for a 5man");
-                	}
-                }
-            }
-        }
-
-        void AfterRemove(AuraEffect const *aurEff, AuraEffectHandleModes mode) {
-            if (GetCaster()) {
-                if (Unit* owner = GetCaster()->GetOwner()) {
-                    if (owner->GetTypeId() == TYPEID_PLAYER) {
-                    	if (Player *player = owner->ToPlayer()) {
-                    		ChatHandler(player->GetSession()).SendSysMessage("Your 5man buff has been removed");
-                    	}
-                    }
-                }
-            }
-        }
-
-		void Register() override {
-            AfterEffectApply += AuraEffectApplyFn(solocraft_aura_5man_AuraScript::AfterApply,
-            		EFFECT_1, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
-            AfterEffectRemove += AuraEffectRemoveFn(solocraft_aura_5man_AuraScript::AfterRemove,
-            		EFFECT_1, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
-		}
-	};
-
-	AuraScript* GetAuraScript() const override {
-		return new solocraft_aura_5man_AuraScript();
-	}
-};
+namespace {
 
 class solocraft_player_instance_handler : public PlayerScript {
 public:
@@ -88,8 +28,16 @@ public:
 	}
 
 	void OnMapChanged(Player *player) override {
-		int difficulty = 1;
 		Map *map = player->GetMap();
+		int difficulty = CalculateDifficulty(map, player);
+		int numInGroup = GetNumInGroup(player);
+		ApplyBuffs(player, map, difficulty, numInGroup);
+	}
+private:
+	std::map<ObjectGuid, int> _unitDifficulty;
+
+	int CalculateDifficulty(Map *map, Player *player) {
+		int difficulty = 1;
 		if (map) {
 			if (map->Is25ManRaid()) {
 				difficulty = 25;
@@ -101,36 +49,57 @@ public:
 				difficulty = 5;
 			}
 		}
+		return difficulty;
+	}
 
+	int GetNumInGroup(Player *player) {
+		int numInGroup = 1;
+		Group *group = player->GetGroup();
+		if (group) {
+			Group::MemberSlotList const& groupMembers = group->GetMemberSlots();
+			numInGroup = groupMembers.size();
+		}
+		return numInGroup;
+	}
+
+	void ApplyBuffs(Player *player, Map *map, int difficulty, int numInGroup) {
+		ClearBuffs(player, map);
 		if (difficulty > 1) {
-			InstanceMap *instanceMap = map->ToInstanceMap();
-			InstanceScript *instanceScript = instanceMap->GetInstanceScript();
-
-			int numInGroup = 1;
-			Group *group = player->GetGroup();
-			if (group) {
-				Group::MemberSlotList const& groupMembers = group->GetMemberSlots();
-				numInGroup = groupMembers.size();
-			}
+			//InstanceMap *instanceMap = map->ToInstanceMap();
+			//InstanceScript *instanceScript = instanceMap->GetInstanceScript();
 
 			ChatHandler(player->GetSession()).PSendSysMessage("Entered %s (difficulty = %d, numInGroup = %d)",
 					map->GetMapName(), difficulty, numInGroup);
 
-			player->AddAura(20217, player);
-			player->AddAura(solocraft_aura_5man::SPELL_ID, player);
-		} else {
-			ChatHandler(player->GetSession()).PSendSysMessage("Left to %s", map->GetMapName());
-			player->RemoveAura(20217);
-			player->RemoveAura(solocraft_aura_5man::SPELL_ID);
+			_unitDifficulty[player->GetGUID()] = difficulty;
+			for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i) {
+				player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_PCT, float(difficulty * 100), true);
+			}
+			player->SetFullHealth();
+			if (player->getPowerType() == POWER_MANA) {
+				player->SetPower(POWER_MANA, player->GetMaxPower(POWER_MANA));
+			}
 		}
 	}
-private:
+
+	void ClearBuffs(Player *player, Map *map) {
+		std::map<ObjectGuid, int>::iterator unitDifficultyIterator = _unitDifficulty.find(player->GetGUID());
+		if (unitDifficultyIterator != _unitDifficulty.end()) {
+			int difficulty = unitDifficultyIterator->second;
+			_unitDifficulty.erase(unitDifficultyIterator);
+
+			ChatHandler(player->GetSession()).PSendSysMessage("Left to %s (removing difficulty = %d)",
+					map->GetMapName(), difficulty);
+
+			for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i) {
+				player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_PCT, float(difficulty * 100), false);
+			}
+		}
+	}
 };
 
 }
 
 void AddSC_solocraft() {
-	new solocraft_aura_5man();
-
 	new solocraft_player_instance_handler();
 }
